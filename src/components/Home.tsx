5d074a361bff03d46ca54d2aac477eabde8987d4
Livesh kumar <57588397+dev-osiris@users.noreply.github.com>
2023-08-27 21:20:59 +0530

    changed background color

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,8 +4,10 @@ function Home() {
     <div className="home">
       <h2>welcome to contact and map app.</h2>
       <h3>
-        Here the first section 'contact' is capable of contact management while the second
-        section is dedicated to covid-19 tracking and maps.
+        Here the first section 'contact' is capable of contact management while the 
+        <br />
+        second section is dedicated to covid-19 tracking and maps.
+        <br />
         More details on how to use this  is availaible in the README.md file.
       </h3>
     </div>
